ebc23a50da14ee3f7c526c610a0be6ed1965855d
Eden Park <jaeseong.park@sysintegrate.co.nz>
2026-01-20 08:30:56 +1300

    feat: enhance app configuration with zone change detection and base URL provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,28 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { routes } from './app.routes';
+import { httpInterceptorProviders } from './core/interceptors';
+
+export function getBaseUrl() {
+  return document.getElementsByTagName('base')[0].href;
+}
+
+const baseUrlProvider = [{ provide: 'BASE_URL', useFactory: getBaseUrl, deps: [] }];
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    // Http providers
+    provideHttpClient(withInterceptorsFromDi()),
+    ...httpInterceptorProviders,
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    ...baseUrlProvider,
+  ],
 };
