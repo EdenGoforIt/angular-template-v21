9405ea5fe24d1cc6ffcf59ad359941e5da85ac75
Eden Park <edengoforit@gmail.com>
2026-01-20 16:41:29 +1300

    feat: remove Dynamic Forms Example component and related routes; update Home component layout and styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,13 +6,6 @@ export const routes: Routes = [
     loadComponent: () => import('./features/home/home.component').then((m) => m.HomeComponent),
   },
   {
-    path: 'dynamic-forms',
-    loadComponent: () =>
-      import('./features/dynamic-forms-example/dynamic-forms-example.component').then(
-        (m) => m.DynamicFormsExampleComponent,
-      ),
-  },
-  {
     path: 'access-denied',
     loadComponent: () =>
       import('./core/components/access-denied/access-denied.component').then(
