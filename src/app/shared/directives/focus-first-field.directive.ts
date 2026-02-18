56323a9cd871f9b5d2aab0707ab224e77e1ee77d
Eden Park <edengoforit@gmail.com>
2026-02-18 18:24:31 +1300

    refactor: replace ngAfterViewInit with afterNextRender in FocusFirstFieldDirective

--- src/app/shared/directives/focus-first-field.directive.ts
+++ src/app/shared/directives/focus-first-field.directive.ts
@@ -1,25 +1,25 @@
-import { AfterViewInit, Directive, ElementRef, inject } from '@angular/core';
+import { afterNextRender, Directive, ElementRef, inject } from '@angular/core';
 
 @Directive({
   // eslint-disable-next-line @angular-eslint/directive-selector
   selector: '[focusFirstField]',
 })
-export class FocusFirstFieldDirective implements AfterViewInit {
+export class FocusFirstFieldDirective {
   #el = inject(ElementRef);
 
-  ngAfterViewInit(): void {
-    const firstMatFormField = this.#el.nativeElement.querySelector('mat-form-field');
+  constructor() {
+    afterNextRender(() => {
+      const firstMatFormField = this.#el.nativeElement.querySelector('mat-form-field');
 
-    if (firstMatFormField) {
-      const focusableElement = firstMatFormField.querySelector(
-        'input, textarea, select, [tabindex]:not([tabindex="-1"])'
-      );
+      if (firstMatFormField) {
+        const focusableElement = firstMatFormField.querySelector(
+          'input, textarea, select, [tabindex]:not([tabindex="-1"])'
+        );
 
-      if (focusableElement) {
-        focusableElement.focus();
-      } else {
-        console.warn('No focusable control found inside the first mat-form-field.');
+        if (focusableElement) {
+          focusableElement.focus();
+        }
       }
-    }
+    });
   }
 }
