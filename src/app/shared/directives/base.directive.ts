8d2609895a88e8df236562f9d23fedc39e34d942
Eden Park <edengoforit@gmail.com>
2026-02-18 18:24:16 +1300

    refactor: remove unused Subject and cleanup logic in BaseDirective

--- src/app/shared/directives/base.directive.ts
+++ src/app/shared/directives/base.directive.ts
@@ -1,17 +1,8 @@
 import { DestroyRef, Directive, inject } from '@angular/core';
-import { Subject } from 'rxjs';
 
 @Directive({
-  selector: '[appBaseDirective]'
+  selector: '[appBaseDirective]',
 })
 export class BaseDirective {
-  protected readonly destroy$ = new Subject<void>();
   protected readonly destroyRef = inject(DestroyRef);
-
-  constructor() {
-    this.destroyRef.onDestroy(() => {
-      this.destroy$.next();
-      this.destroy$.complete();
-    });
-  }
 }
