7f30802c6b4c48a1ad5c95cfa5944ad3b2794ba0
Eden Park <edengoforit@gmail.com>
2026-02-19 17:54:56 +1300

    refactor: improve type safety and simplify logic in FilterPipe transform method

--- src/app/shared/pipes/filter.pipe.ts
+++ src/app/shared/pipes/filter.pipe.ts
@@ -1,22 +1,18 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Pipe, PipeTransform } from '@angular/core';
+
 @Pipe({
   name: 'filter',
   pure: false,
-  standalone: true
 })
-
-/**
- * Filter a KeyValue json list
- * To work with arrays it needs to be placed after a 'keyvalue' pipe
- */
 export class FilterPipe implements PipeTransform {
-  transform(items: any[], search: { [key: string]: any }): any[] {
+  transform<T extends Record<string, unknown>>(items: T[], search: Record<string, string>): T[] {
     return items
       ? items.filter((item) => {
           return Object.keys(search).find((key) => {
-            return search[key]
-              ? !!item[key] && item[key].toLowerCase().indexOf(search[key].toLowerCase()) !== -1
+            const searchValue = search[key];
+            const itemValue = item[key];
+            return searchValue
+              ? typeof itemValue === 'string' && itemValue.toLowerCase().includes(searchValue.toLowerCase())
               : true;
           });
         })
